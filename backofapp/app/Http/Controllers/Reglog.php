828f6567e7cc69317dcbfec6d5816d105a4aa004
[Mohamed Zakane] <[zakane993@gmail.com]>
2026-07-22 15:37:35 +0100

    Login working and signup working

--- backofapp/app/Http/Controllers/Reglog.php
+++ backofapp/app/Http/Controllers/Reglog.php
@@ -1,34 +1,45 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class Reglog extends Controller
 {
     public function register(Request $request)
     {
-            DB::table('accounts')->insert([
-                'name' => $request->input('Name'),
-                'last_name' => $request->input('LastName'),
-                'email' => $request->input('Email'),
-                'phone_number' => $request->input('PhoneNumber'),
-                'password' => $request->input('Password'),
-                'created_at' => now(),
-            ]);
-            return response()->json(['message' => 'User registered successfully']);
-    }
-     public function login(Request $request)
-    {
-        $email = $request->input('Email');
-        $password = $request->input('Password');
+        DB::table('accounts')->insert([
+            'name' => $request->input('Name'),
+            'last_name' => $request->input('LastName'),
+            'email' => $request->input('Email'),
+            'phone_number' => $request->input('PhoneNumber'),
+            'password' => $request->input('Password'),
+            'created_at' => now(),
+        ]);
 
-        $user = DB::table('accounts')->where('email', $email)->first();
-        if ($user && $user->password === $password) {
-            // User authenticated successfully
-            return response()->json(['message' => 'Login successful']);
+        return response()->json([
+            'message' => 'Account created successfully'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $account = DB::table('accounts')
+            ->where('email', $request->input('email'))
+            ->where('password', $request->input('password'))
+            ->first();
+
+        if ($account) {
+            return response()->json([
+                'check'=> true,
+                'message' => 'Connexion réussie'
+            ]);
         } else {
-            // Invalid credentials
-            return response()->json(['message' => 'Invalid email or password'], 401);
+            return response()->json([
+                'check'=> false,
+                'message' => 'Connexion échouée'
+            ]);
         }
     }
 }
